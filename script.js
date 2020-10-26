10d186086434a89563b7e1325e748fd636bf9c7d
Feeps <feepsmail@gmail.com>
2020-10-26 14:45:24 +0100

    Working version

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ const FAPicker = (function(){
 	self.Picker = class {
 		constructor(){
 			this.categories = [];
+			this.selectedCategories = [];
+			this.searchValue = "";
+			this.selectedInput = null;
 			
 			var pickerDiv = document.createElement("div");
 			pickerDiv.classList.add("faPicker");
@@ -24,13 +27,22 @@ const FAPicker = (function(){
 			this.searchBar.setAttribute("placeholder", "Search...");
 			this.pickerDiv.appendChild(this.searchBar);
 			
+			var self = this;
+			const inputHandler = function(e){
+				const val = e.target.value;
+				self.searchValue = val.toLowerCase().trim();
+				self.updateIcons();
+			}
+			this.searchBar.addEventListener("input", inputHandler);
+			this.searchBar.addEventListener("propertychange", inputHandler); // IE8
+			
 			var group = document.createElement("div");
 			group.classList.add("faPicker-container");
 			this.pickerDiv.appendChild(group);
 			
-			this.categoriesList = document.createElement("div");
-			this.categoriesList.classList.add("faPicker-categoriesList", "faPicker-hideScroll");
-			group.appendChild(this.categoriesList);
+			this.catList = document.createElement("div");
+			this.catList.classList.add("faPicker-categoriesList", "faPicker-hideScroll");
+			group.appendChild(this.catList);
 			
 			this.iconsDiv = document.createElement("div");
 			this.iconsDiv.classList.add("faPicker-iconsList");
@@ -45,8 +57,7 @@ const FAPicker = (function(){
 				
 				//Display
 				pickerDiv.style.display = "block";
-				
-				
+				self.selectedInput = this;
 			};
 		}
 		
@@ -54,13 +65,52 @@ const FAPicker = (function(){
 			this.categories.push(category);
 		}
 		
-		// This function rebuild the DOM objects.
 		update(){
-			this.iconsDiv.innerHTML = ""; // Reset the div
-			this.categoriesList.innerHTML = "";
+			this.updateCategories();
+			this.updateIcons();
+		}
+		
+		updateCategories(){
+			while (this.catList.hasChildNodes()) {
+				this.catList.removeChild(this.catList.lastChild); // Remove children
+			}
 			
 			var ul = document.createElement("ul");
-			this.categoriesList.appendChild(ul);
+			
+			for(const key in this.categories){
+				var category = this.categories[key];
+				
+				var li = document.createElement("li");
+				li.innerHTML = category.name;
+				
+				var self = this;
+				li.addEventListener("click", function(){ // Add click listener here
+					if (this.classList.contains("active")){
+						this.classList.remove("active");
+						
+						const index = self.selectedCategories.indexOf(key);
+						if (index > -1) {
+							self.selectedCategories.splice(index, 1);
+						}
+					}else{
+						this.classList.add("active");
+						self.selectedCategories.push(key);
+					}
+					
+					self.updateIcons();
+				});
+				
+				ul.appendChild(li);
+			}
+			
+			this.catList.appendChild(ul);
+		}
+		
+		// This function rebuild the DOM objects.
+		updateIcons(){
+			while (this.iconsDiv.hasChildNodes()) {
+				this.iconsDiv.removeChild(this.iconsDiv.lastChild); // Remove children
+			}
 			
 			var test  = document.createElement("div");
 			var table = document.createElement("table");
@@ -69,15 +119,41 @@ const FAPicker = (function(){
 			
 			var createdIcons = 0;
 			var tr;
-			for(var category of this.categories){
+			for(var catKey in this.categories){
+				var category = this.categories[catKey];
 				
-				var li = document.createElement("li");
-				ul.appendChild(li);
-				li.innerHTML = category.name;
-				
-				
+				if(this.searchValue.length === 0){
+					if(this.selectedCategories.length !== 0){
+						if(!this.selectedCategories.includes(catKey)){
+							continue;
+						}
+					}
+				}
+
+
 				for(var key in category.icons){
-					var icon = category.icons[key];
+					const icon = category.icons[key];
+					
+					
+					if(this.searchValue.length !== 0){
+						var found = false;
+						
+						if(icon.name.indexOf(this.searchValue) > -1){
+							found = true;
+						}else{
+							for(const searches of icon.searchTerms){
+								if (searches.indexOf(this.searchValue) > -1) {
+									found = true;
+									break;
+								}
+							}
+						}
+						
+						if(!found){
+							continue;
+						}
+					}
+	
 					
 					for(var style of icon.styles){
 						if(createdIcons % 6 == 0){
@@ -86,17 +162,22 @@ const FAPicker = (function(){
 						}
 						
 						// Create the icon DOM
-						var td = document.createElement("td");
+						const td = document.createElement("td");
 						tr.appendChild(td);
 						
-						var btn = document.createElement("button");
+						const btn = document.createElement("button");
 						btn.classList.add("faPicker-btn");
 						td.appendChild(btn);
 
-						var i = document.createElement("i");
+						const i = document.createElement("i");
 						i.classList.add(corresponding_classes[style]);
 						i.classList.add("fa-" + icon.name);
 						btn.appendChild(i);
+						
+						var self = this;
+						btn.addEventListener("click", function(){ // Add click listener here
+							self.selectedInput.value = i.className;
+						});
 						
 						createdIcons += 1;
 					}
