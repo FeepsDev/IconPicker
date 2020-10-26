ccece47978335ed860ef43299b3bd188a3b6b3a3
Feeps <feepsmail@gmail.com>
2020-10-26 11:42:10 +0100

    Rewrited

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const FAPicker = (function(){
+	var self = {};
+	
 	var corresponding_classes = {
 		"solid": "fas",
 		"regular": "far",
@@ -7,108 +9,139 @@ const FAPicker = (function(){
 		"brands": "fab"
 	};
 	
-    return function(options={searchBar: false}){
-		var pickerDiv = document.createElement("div");
-		pickerDiv.classList.add("faPicker");
-		document.body.appendChild(pickerDiv);
-		
-		var icons = [];
-		
-		const clickListener = function(){
-			// Moving
-			var rect = this.getBoundingClientRect();
-			pickerDiv.style.top = (rect.top + this.offsetHeight) + "px";
-			pickerDiv.style.left = rect.left + "px";
+	self.Picker = class {
+		constructor(){
+			this.categories = [];
 			
-			//Display
-			pickerDiv.style.display = "block";
+			var pickerDiv = document.createElement("div");
+			pickerDiv.classList.add("faPicker");
+			document.body.appendChild(pickerDiv);
+			this.pickerDiv = pickerDiv;
+			
+			this.searchBar = document.createElement("input");
+			this.searchBar.classList.add("faPicker-searchBar");
+			this.searchBar.setAttribute("type", "text");
+			this.searchBar.setAttribute("placeholder", "Search...");
+			this.pickerDiv.appendChild(this.searchBar);
+			
+			var group = document.createElement("div");
+			group.classList.add("faPicker-container");
+			this.pickerDiv.appendChild(group);
+			
+			this.categoriesList = document.createElement("div");
+			this.categoriesList.classList.add("faPicker-categoriesList", "faPicker-hideScroll");
+			group.appendChild(this.categoriesList);
+			
+			this.iconsDiv = document.createElement("div");
+			this.iconsDiv.classList.add("faPicker-iconsList");
+			group.appendChild(this.iconsDiv);
+			
+			this.clickListener = function(){
+				console.log("Clicked");
+				
+				var rect = this.getBoundingClientRect();
+				pickerDiv.style.top = (rect.top + this.offsetHeight) + "px";
+				pickerDiv.style.left = rect.left + "px";
+				
+				//Display
+				pickerDiv.style.display = "block";
+				
+				
+			};
 		}
 		
-		this.update = function(){
-			// Build DOM elements
+		addCategory(category){
+			this.categories.push(category);
+		}
+		
+		// This function rebuild the DOM objects.
+		update(){
+			this.iconsDiv.innerHTML = ""; // Reset the div
+			this.categoriesList.innerHTML = "";
+			
+			var ul = document.createElement("ul");
+			this.categoriesList.appendChild(ul);
+			
+			var test  = document.createElement("div");
 			var table = document.createElement("table");
-			var tBody = document.createElement("tbody");
+			var tBody = document.createElement("tBody");
 			table.appendChild(tBody);
 			
-			var i = 0;
+			var createdIcons = 0;
+			var tr;
+			for(var category of this.categories){
 				
-			var tr = document.createElement("tr");
-			tBody.appendChild(tr);
-			console.log(icons);
-			
-		$.getJSON("https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/metadata/icons.json", function(data) {
-			
-			for(var key in data) {
-
-				for(var style in data[key].styles){
-					if (i == 4){
-						tr = document.createElement("tr");
-						tBody.appendChild(tr);
+				var li = document.createElement("li");
+				ul.appendChild(li);
+				li.innerHTML = category.name;
+				
+				
+				for(var key in category.icons){
+					var icon = category.icons[key];
+					
+					for(var style of icon.styles){
+						if(createdIcons % 6 == 0){
+							tr = document.createElement("tr");
+							tBody.appendChild(tr);
+						}
 						
-						i = 0;
+						// Create the icon DOM
+						var td = document.createElement("td");
+						tr.appendChild(td);
+						
+						var btn = document.createElement("button");
+						btn.classList.add("faPicker-btn");
+						td.appendChild(btn);
+
+						var i = document.createElement("i");
+						i.classList.add(corresponding_classes[style]);
+						i.classList.add("fa-" + icon.name);
+						btn.appendChild(i);
+						
+						createdIcons += 1;
 					}
-					var iconD = document.createElement("td");
-					tr.appendChild(iconD);	
-					iconD.innerHTML = '<button style="width:100%;heigth:100%"><i class="' + corresponding_classes[data[key].styles[style]] + ' fa-' + key + '"></i></button>	';
-					i += 1;
-				}
-			} 
-		});
-			icons.forEach(function(icon){
-				console.log("ic");
-
-			});
-
-			
-			pickerDiv.appendChild(table);
-		}
-		
-		this.setIcons = function(i){
-			console.log(i);
-			icons = i;
-		}
-		
-		this.enable = function(domObj){
-			domObj.addEventListener("click", clickListener);
-		}
-		
-		this.disable = function(domObj){
-			domObj.removeEventListener("click", clickListener);
-		}
-		
-		// Icon object
-		this.Icon = (function(){
-			function Icon(faName, availableStyles, searchTerms){
-				this.name = faName;
-				this.availableStyles = availableStyles;
-				this.searchTerms = searchTerms;
-			}
-			
-			return Icon;
-		})();
-		
-		// Category object
-		this.Category = (function(){
-			function Category(name, icons=[]){
-				this.name = name;
-				this.icons = icons;
-			}
-			
-			Category.addIcon = function(icon){
-				this.icons[icon.name] = icon;
-			};
-			
-			Category.removeIcon = function(icon){
-				if(typeof icon == "string"){
-					this.icons[icon] = null;
-				}else{
-					this.icons[icon.name] = null;
 				}
 			}
 			
-			return Category;
-		})();
-	};
+			test.appendChild(table);
+			this.iconsDiv.appendChild(test);
+		}
+		
+		enable(domObj){
+			domObj.addEventListener("click", this.clickListener);
+		}
+		
+		disable(domObj){
+			domObj.addEventListener("click", this.clickListener);
+		}
+	}
+	
+	self.Icon = class {
+		constructor(name, styles, searchTerms){
+			this.name = name;
+			this.styles = styles;
+			this.searchTerms = searchTerms;
+		}
+	}
+	
+	self.Category = class {
+		constructor(name, icons=[]){
+			this.name = name;
+			this.icons = icons;
+		}
+		
+		addIcon(icon){
+			this.icons[icon.name] = icon;
+		}
+		
+		removeIcon(icon){
+			if(typeof icon == "string"){
+				this.icons[icon] = null;
+			}else{
+				this.icons[icon.name] = null;
+			}
+		}
+	}
+
+	return self;
 })();
-
-
